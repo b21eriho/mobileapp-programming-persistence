3ea157b1ccfecfc685da01b4db1667a2581e89b9
Erik Holmberg <b21eriho@student.his.se>
2022-05-12 17:45:03 +0200

    Implement buttons and take screenshot

--- app/src/main/java/com/example/persistence/MainActivity.java
+++ app/src/main/java/com/example/persistence/MainActivity.java
@@ -2,10 +2,16 @@ package com.example.persistence;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ContentValues;
+import android.database.Cursor;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -36,6 +42,37 @@ public class MainActivity extends AppCompatActivity {
 
         db = new DataBaseHelper(this);
 
+        writeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                ContentValues values = new ContentValues();
+                values.put(DataBaseHelper.COLLUMN_NAME, String.valueOf(nameInput.getText()));
+                values.put(DataBaseHelper.COLLUMN_BRAND, String.valueOf(brandInput.getText()));
+                values.put(DataBaseHelper.COLLUMN_NR_WHEELS, String.valueOf(nrWheelsInput.getText()));
+                db.getWritableDatabase().insert(DataBaseHelper.TABLE_BIKE, null, values);
+
+            }
+        });
+
+        readButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Cursor cursor = db.getReadableDatabase().rawQuery("SELECT * FROM " + DataBaseHelper.TABLE_BIKE + " ORDER BY " + DataBaseHelper.COLLUMN_ID, null, null);
+
+                String output = "";
+
+                while (cursor.moveToNext()) {
+                    output += "Bike. Name:" + cursor.getString(cursor.getColumnIndexOrThrow(DataBaseHelper.COLLUMN_NAME)) + " Brand:" +
+                    cursor.getString(cursor.getColumnIndexOrThrow(DataBaseHelper.COLLUMN_BRAND)) + " Number of wheels:" +
+                    cursor.getString(cursor.getColumnIndexOrThrow(DataBaseHelper.COLLUMN_NR_WHEELS)) + "\n";
+                }
+                cursor.close();
+                outputField.setText(output);
+
+            }
+        });
 
     }
 }
